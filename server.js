f10d6179acb55054870a513a05e2b82a41ff3da6
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-07 14:43:17 -0800

    server static files for root directory(homepage)

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const port = process.env.port || 3001;
 const path = require('path');
 
 // SECTION - Middleware
+app.use(express.static(`${__dirname}/public`));
 
 // SECTION - Routes
 app.get('/', (req, res) => {
-    res.send('The root, bb!');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 // SECTION - Server Start
