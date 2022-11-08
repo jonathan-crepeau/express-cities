62a07a7e2d1ae2bd4ca11157012fd28e2d48d6fe
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-08 11:18:08 -0800

    initial express setup and start server complete

--- server.js
+++ server.js
@@ -1,49 +1,21 @@
-const { response } = require('express');
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require('path');
-
-// SECTION - Initialize Database
-const db = require('./models');
-
-// SECTION - TEMP DATA
-const TEMP_CITIES = [
-  {name: 'San Francisco', description: 'The Best!'},
-  {name: 'San Diego', description: 'Beaches!'},
-  {name: 'Seatle', description: 'Rain!'},
-];
-
 
 // SECTION - Middleware
-// NOTE - Serve Public Assets
 app.use(express.static(`${__dirname}/public`));
-
-// NOTE - BodyParser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 
 // SECTION - Routes
+
+// Homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-app.get('/cities', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/citiesIndex.html'));
+    res.send('The homepage.');
 });
 
 
-// SECTION - JSOn Data Endpoints Below
-
-// NOTE - Get Cities Index
-app.get('/api/v1/cities', (req, res) => {
-    res.send('hello')
-})
-
-
-// SECTION - Server Start
+// SECTION - Start Server
 app.listen(PORT, () => {
     console.log(`Application is listening on port ${PORT}.`);
 });
